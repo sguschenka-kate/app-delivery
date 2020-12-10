570ad49cd05bc24332413f0d7232b315c58132d1
Kateryna Kochubei <kochubej.katerina@gmail.com>
2020-12-10 14:32:55 +0200

    my app first commit

--- src/components/Main/ProductItem/index.js
+++ src/components/Main/ProductItem/index.js
@@ -1,6 +1,24 @@
-function ProductItem() {
+
+import "./style.scss";
+
+function ProductItem(props) {
+  const { type, id, name, time, rate, price, image } = props;
+  
+
   return (
-    <li className="product__item"></li>
+    <li className="product__item">
+      <img src={image} className="product__image" alt={name} />
+      <div className="product__info">
+        <h3 className="product__name">
+          { name }
+        </h3>
+        <div className="product__meta">
+          <span className="product__meta-time">{ time }</span>
+          <span className="product__meta-rank">{ rate }</span>
+        </div>
+        <div className="product__price">{ price }</div>
+      </div>
+    </li>
   )
 }
 
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,29 +1,53 @@
-import { useState, useEffect } from "react";
-import { Loader } from "../shared/Loader";
-import { ProductList } from "./ProductList";
-import * as api from "../../api/products";
+// import { useState, useEffect } from "react";
+// import { Loader } from "../shared/Loader";
+import { ProductItem } from "./ProductItem";
+// import * as api from "../../api/products";
 
 import "./style.scss";
 
 function Main() {
-  const [products, setProducts] = useState({});
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetchProducts()
-  }, [])
-
-  const fetchProducts = async () => {
-    setLoading(true);
-    const response = await api.fetchProducts();
-    setProducts(response);
-    setLoading(false);
+  // const [products, setProducts] = useState({});
+  const products = {
+    1: {id: Date.now(), type: "burger", image: "/img/burger.png", name: "Smash burger", price: 10.49, rate: 5.0, time: "30 min", added: false},
+    2: {id: Date.now(), type: "drink", image: "/img/burger.png", name: "Soda", price: 10.49, rate: 4.9, time: "15 min", added: false},
+    3: {id: Date.now(), type: "chiabata", image: "/img/burger.png", name: "Chiabata with pork", price: 10.49, rate: 4.6, time: "25 min", added: false},
   }
+
+  // const [isLoading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   fetchProducts()
+  // }, [])
+
+  // const fetchProducts = async () => {
+  //   setLoading(true);
+  //   const response = await api.fetchProducts();
+  //   setProducts(response);
+  //   setLoading(false);
+  // }
 
   return (
     <div className="main">
-      {isLoading && <Loader />}
-      {!isLoading && <ProductList />}
+      {/* {isLoading && <Loader />} */}
+      <div className="main__container">
+        {/* {!isLoading &&  */}
+          <ul className="product__list">
+            {Object.values(products).map((product) =>
+              <ProductItem
+              type={product.type}
+              name={product.name}
+              id={product.id}
+              image={product.image}
+              price={product.price}
+              rate={product.rate}
+              time={product.time}
+              key={product.id}
+            />
+            )}
+          </ul>
+        {/* } */}
+      </div>
+
 
     </div>
   )
