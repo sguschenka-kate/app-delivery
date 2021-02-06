d7b8aa0e0d56f675dd313561d389631af41abef0
Kate <kochubej.katerina@gmail.com>
2021-02-06 15:17:48 +0200

    update

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -3,7 +3,7 @@ import "./style.scss";
 function Loader() {
   return (
     <div className="loader">
-      <img src="./img/loader-man.png" alt="logo" className="loader__img" />
+      <img src="/delivery-app/img/loader-man.png" alt="logo" className="loader__img" />
       <progress className="loader__progress matter-progress-circular"></progress>
     </div>
   )
